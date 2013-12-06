d973732aa14bb5073a6bd20bf193bfe349d06a36
bszggg <veszti@freemail.hu>
2013-12-06 09:36:01 +0100

    My modifieds
    bszggg first test modd

---
+++ bszggg_newfile_test.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+<div id="pagewrap">
+<ul id="summer-drinks">
+   <li class="favorite">Whiskey and Ginger Ale</li>
+   <li>Wheat Beer</li>
+   <li>Mint Julip</li>
+</ul>
+</div>
+
+
+<style type="text/css">
+
+
+html body div#pagewrap ul#summer-drinks li.favorite {
+  color: red;
+  font-weight: bold;
+}
+
+.favorite {
+  color: red !important;
+  font-weight: bold !important;
+}
+.last {
+   margin-right: 0 !important;
+}
+</style>
+</head>
+</html>
